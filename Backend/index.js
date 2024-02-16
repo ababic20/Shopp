aaaabec17c8a8edd46e354ed874f1cb1d2d27348
ababic20 <ababic20@foi.hr>
2024-02-16 13:19:43 +0100

    Successful creation and addition of products to the database

--- Backend/index.js
+++ Backend/index.js
@@ -24,6 +24,84 @@ app.get("/", (req,res)=>{
     res.send("Express app is running")
 })
 
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({storage:storage})
+
+//Endpoint for images
+app.use('/images',express.static('upload/images'))
+app.post("/upload", upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}` 
+    })
+})
+
+//Creating products
+const Product = mongoose.model("Product", {
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    avilable:{
+        type:Boolean,
+        default:true,
+    }
+})
+
+app.post('/addproduct', async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0){
+        
+    }
+    const product = new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+
+
 app.listen(port, (err) => {
     if (!err) {
         console.log("Server running on port " + port);
