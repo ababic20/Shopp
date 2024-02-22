0ec7d6a140e22e0d977ef026a9560327a844b543
ababic20 <ababic20@foi.hr>
2024-02-22 15:51:41 +0100

    Designed registration form

--- Frontend/my-react-app/src/Pages/LoginSignup.jsx
+++ Frontend/my-react-app/src/Pages/LoginSignup.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Registriraj se</h1>
+          <div className="loginsignup-fields">
+            <input type="text" placeholder='Vaše ime'/>
+            <input type="email" placeholder='Email adresa' />
+            <input type="password" placeholder='Lozinka' />
+          </div>
+          <button>Nastavi</button>
+          <p className='loginsignup-login'>Već imate račun? <span>Prijavi se ovdje!</span></p>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name='' id='' />
+            <p>Prijavom prihvaćam uvjete korištenja i politika privatnosti</p>
+          </div>
+      </div>
+    </div>
   )
 }
