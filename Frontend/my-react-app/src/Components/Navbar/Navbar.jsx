5ea21f009415c49d2102a2bba4c15179f9d16367
ababic20 <ababic20@foi.hr>
2024-03-02 22:06:58 +0100

    added login and signup functions, fetch is used to send req to the endpoint, added rendering of the component depending on the 'state' state, formdata contains data from the form. application and registration completed

--- Frontend/my-react-app/src/Components/Navbar/Navbar.jsx
+++ Frontend/my-react-app/src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,38 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { Link } from 'react-router-dom'; 
 import './Navbar.css';
 import logo from '../Images/logo.png';
 import cart_icon from '../Images/cart_icon.png';
 import { ShopContext } from '../../Context/ShopContext';
+import arrow2 from '../Images/arrow2.png'
 
 export const Navbar = () => {
   const [menu, setMenu] = useState("shop");
-  const {getTotalCartItems }= useContext(ShopContext)
+  const {getTotalCartItems }= useContext(ShopContext);
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) =>{
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+    
+  }
   return (
     <div className='navbar'>
       <div className="nav-logo">
         <img src={logo} alt="" />
         <p>Bostan</p>
       </div>
-      <ul className="nav-menu">
+      <img className='dropdown'onClick={dropdown_toggle} src={arrow2} alt="" />
+      <ul className="nav-menu" ref={menuRef}>
         <li onClick={() => { setMenu("shop") }}><Link to="/" style={{textDecoration:'none'}}>Naslovna</Link>{menu === "shop" ? <hr /> : <></>}</li>
         <li onClick={() => { setMenu("mens") }}><Link to="/mens" style={{textDecoration:'none'}}>Muškarci</Link>{menu === "mens" ? <hr /> : <></>}</li>
         <li onClick={() => { setMenu("womens") }}><Link to="/womens" style={{textDecoration:'none'}}>Žene</Link>{menu === "womens" ? <hr /> : <></>}</li>
         <li onClick={() => { setMenu("kids") }}><Link to="/kids" style={{textDecoration:'none'}}>Djeca</Link>{menu === "kids" ? <hr /> : <></>}</li>
       </ul>
       <div className='nav-login-cart'>
-        <Link to='/login'><button>Prijava</button></Link>
+        {localStorage.getItem('auth-token')?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>
+        Odjavi se</button>:<Link to='/login'><button>Prijava</button></Link>}
+        
         <Link to='/cart'><img src={cart_icon} alt="Cart" /></Link>
         <div className='nav-cart-count'>{getTotalCartItems()}</div>
       </div>
--- Frontend/my-react-app/src/Pages/LoginSignup.jsx
+++ Frontend/my-react-app/src/Pages/LoginSignup.jsx
@@ -1,18 +1,76 @@
 import React from 'react'
 import './CSS/LoginSignup.css'
+import { useState } from 'react'
 
 export const LoginSignup = () => {
+
+  const[state,setState] = useState("Prijavi se");
+  const[formData,setformdata,] = useState({
+    username:"",
+    password:"",
+    email:""
+  });
+
+  const changeHandler = (e)=>{
+    setformdata({...formData,[e.target.name]:e.target.value})
+
+  }
+
+  const login = async()=>{
+    console.log("Login funkcija izvršena", formData);
+    let responsedata;
+    await fetch('http://localhost:4000/login',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((res)=> res.json()).then((data)=>responsedata=data)
+
+    if(responsedata.success){
+      localStorage.setItem('auth-token', responsedata.token);
+      window.location.replace("/")
+    }
+    else{
+      alert(responsedata.errors)
+    }
+  }
+
+  const signup = async()=>{
+    console.log("Register funkcija izvršena", formData);
+    let responsedata;
+    await fetch('http://localhost:4000/signup',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((res)=> res.json()).then((data)=>responsedata=data)
+
+    if(responsedata.success){
+      localStorage.setItem('auth-token', responsedata.token);
+      window.location.replace("/")
+    }
+    else{
+      alert(responsedata.errors)
+    }
+    }
+ 
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Registriraj se</h1>
+        <h1>{state}</h1>
           <div className="loginsignup-fields">
-            <input type="text" placeholder='Vaše ime'/>
-            <input type="email" placeholder='Email adresa' />
-            <input type="password" placeholder='Lozinka' />
+            {state==="Registriraj se"?<input type="text" placeholder='Vaše ime' name='username' value={formData.username} onChange={changeHandler}/>:<></>}
+            <input type="email" placeholder='Email adresa' name='email' value={formData.email} onChange={changeHandler} />
+            <input type="password" placeholder='Lozinka' name='password' value={formData.password} onChange={changeHandler} />
           </div>
-          <button>Nastavi</button>
-          <p className='loginsignup-login'>Već imate račun? <span>Prijavi se ovdje!</span></p>
+          <button onClick={()=>{state==="Prijavi se"?login():signup()}}>Nastavi</button>
+          {state==="Registriraj se"?<p className='loginsignup-login'>Već imate račun? <span onClick={()=>{setState("Prijavi se")}}>Prijavi se ovdje!</span></p>:<></>}
+          {state==="Prijavi se"?<p className='loginsignup-login'>Napravi svoj račun?<span onClick={()=>{setState("Registriraj se")}}>Klikni ovdje!</span></p>:<></>}
           <div className="loginsignup-agree">
             <input type="checkbox" name='' id='' />
             <p>Prijavom prihvaćam uvjete korištenja i politika privatnosti</p>
